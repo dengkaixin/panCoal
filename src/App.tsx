c6fccf0c8b55fe94151c61e48c762172f2617b8b
dengkaixin <1045316631@qq.com>
2024-04-16 14:11:35 +0800

    business

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/*
+ * @Author: dengkaixin
+ * @Descripttion: dengkaixin的代码
+ * @Date: 2024-04-12 09:52:50
+ * @LastEditors: dengkaixin
+ * @LastEditTime: 2024-04-16 11:33:40
+ */
+import { ConfigProvider } from 'antd';
+import MainPage from './business/mainPage';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#1982E4',
+          borderRadius: 5,
+          colorBgContainer: '#081A36',
+          colorBorder: '#0C547E',
+          colorText: '#fff',
+        },
+        components: {
+          Slider: {
+            handleSize: 8,
+            handleColor: '#1982E4',
+            trackBg: '#1982E4',
+            railBg: '#003D75',
+            /* 这里是你的组件 token */
+          },
+        },
+      }}>
+      <MainPage/>
+    </ConfigProvider>
   )
 }
 
